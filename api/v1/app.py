c6ed2c0ea432e57a66a7d418d0950b443b730c41
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-04-25 20:55:12 +0400

    created first endpoint

---
+++ api/v1/app.py
@@ -0,0 +1,21 @@
+import os
+
+from api.v1.views import app_views
+from flask import Flask
+from models import storage
+
+app = Flask(__name__)
+
+HOST = os.getenv("HBNB_API_HOST", default="0.0.0.0")
+PORT = os.getenv("HBNB_API_PORT", default=5000)
+
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def close_session(exception):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(HOST, PORT)
