71082fc76b94b480430de2ec6391c06ff3398425
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-04-25 21:06:42 +0400

    created docs for module

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,4 +1,19 @@
 #!/usr/bin/python3
+"""
+This module contains the Flask application for the AirBnB clone API.
+
+It creates a Flask app, registers the blueprint for the API views, and sets up
+the host and port for the app to run.
+
+Functions:
+    close_session: A decorator function to close the database session after
+    each request.
+
+Variables:
+    HOST: The host IP address for the app to run on.
+    PORT: The port number for the app to run on.
+"""
+
 import os
 
 from api.v1.views import app_views
@@ -15,6 +30,12 @@ app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def close_session(exception):
+    """
+    Closes the database session after each request.
+
+    Args:
+        exception: The exception raised during the request, if any.
+    """
     storage.close()
 
 
