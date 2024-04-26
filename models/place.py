eaeda0fa22890d9504f8d87d234d0c1819df4c40
heydarov93 <heyderov.yashar@mail.ru>
2024-04-27 00:54:20 +0400

    else statement added for reviews, amenities, getters and setters

--- models/place.py
+++ models/place.py
@@ -35,30 +35,30 @@ class Place(BaseModel, Base):
 
     __tablename__ = "places"
 
-    city_id: Mapped[str] = mapped_column(
-        String(60), ForeignKey("cities.id"), nullable=False
-    )
-    user_id: Mapped[str] = mapped_column(
-        String(60), ForeignKey("users.id"), nullable=False
-    )
-    name: Mapped[str] = mapped_column(String(128), nullable=False)
-    description: Mapped[str] = mapped_column(String(1024), nullable=True)
-    number_rooms: Mapped[int] = mapped_column(default=0, nullable=False)
-    number_bathrooms: Mapped[int] = mapped_column(default=0, nullable=False)
-    max_guest: Mapped[int] = mapped_column(default=0, nullable=False)
-    price_by_night: Mapped[int] = mapped_column(default=0, nullable=False)
-    latitude: Mapped[float] = mapped_column(nullable=True)
-    longitude: Mapped[float] = mapped_column(nullable=True)
-    reviews = relationship("Review", backref="place", cascade="delete")
-    amenities = relationship(
-        "Amenity", secondary=place_amenity, viewonly=False,
-        overlaps="place_amenities"
-    )
-
-    amenity_ids = []
-
     if os.getenv("HBNB_TYPE_STORAGE", None) != "db":
 
+        city_id: Mapped[str] = mapped_column(
+            String(60), ForeignKey("cities.id"), nullable=False
+        )
+        user_id: Mapped[str] = mapped_column(
+            String(60), ForeignKey("users.id"), nullable=False
+        )
+        name: Mapped[str] = mapped_column(String(128), nullable=False)
+        description: Mapped[str] = mapped_column(String(1024), nullable=True)
+        number_rooms: Mapped[int] = mapped_column(default=0, nullable=False)
+        number_bathrooms: Mapped[int] = mapped_column(default=0,
+                                                      nullable=False)
+        max_guest: Mapped[int] = mapped_column(default=0, nullable=False)
+        price_by_night: Mapped[int] = mapped_column(default=0, nullable=False)
+        latitude: Mapped[float] = mapped_column(nullable=True)
+        longitude: Mapped[float] = mapped_column(nullable=True)
+        reviews = relationship("Review", backref="place", cascade="delete")
+        amenities = relationship(
+            "Amenity", secondary=place_amenity, viewonly=False,
+            overlaps="place_amenities"
+        )
+
+    else:
         @property
         def reviews(self):
             """Get a list of all linked Reviews."""
@@ -81,3 +81,5 @@ class Place(BaseModel, Base):
         def amenities(self, value):
             if isinstance(value, Amenity):
                 self.amenity_ids.append(value.id)
+
+    amenity_ids = []
