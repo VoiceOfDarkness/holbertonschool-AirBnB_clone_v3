25008e0f2cf56e6f3e16ea40b0acebbae0081b87
Abilhasan Samadov <abil.samedov2016@yandex.ru>
2024-04-26 17:38:53 +0400

    docs fix

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+endpoint for states
+"""
 import json
 
 from flask import jsonify
@@ -16,7 +19,7 @@ def states():
     return jsonify([state.to_dict() for state in states.values()]), 200
 
 
-@app_views.route("/states/<state_id:int>", strict_slashes=False)
+@app_views.route("/statese_id:int>", strict_slashes=False)
 def state(state_id: int):
     """Return a state"""
     state = storage.get(State, state_id)
@@ -37,7 +40,8 @@ def delete_state(state_id: int):
     return jsonify({}), 200
 
 
-@app_views.route("/states/<state_id:int>:int", methods=["POST"],
+@app_views.route("/states/<state_id:int>", methods=["POST"
+                ],
                  strict_slashes=False)
 def update_state(state_id: int):
     """Update a state"""
@@ -54,8 +58,10 @@ def update_state(state_id: int):
 
     for key, value in data.items():
         if key not in ["id", "created_at", "updated_at"]:
-            setattr(state, key, value)
+            setattr(state,
+                 key, value)
 
     storage.save()
 
-    return jsonify(state.to_dict()), 200
+    return jsonify(state.to_d), 200
+ state"""
