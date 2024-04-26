2456a3851186c8908646f38f4ab5b1169faa9e1b
heydarov93 <heyderov.yashar@mail.ru>
2024-04-27 00:07:00 +0400

    view for Amenity objects that handles all default RESTFul API actions

---
+++ api/v1/views/amenities.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+"""
+endpoint for states
+"""
+from flask import jsonify
+from flask import request
+
+from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+
+
+@app_views.route("/amenities", strict_slashes=False)
+def amenities():
+    """Return all amenities"""
+    amenities = storage.all(Amenity)
+    return jsonify([amenity.to_dict() for amenity in amenities.values()]), 200
+
+
+@app_views.route("/amenities/<amenity_id>", strict_slashes=False)
+def amenity(amenity_id: str):
+    """Return a amenity"""
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        return jsonify({"error": "Not found"}), 404
+    return jsonify(amenity.to_dict()), 200
+
+
+@app_views.route("/amenities/<amenity_id>", methods=["DELETE"],
+                 strict_slashes=False)
+def delete_amenity(amenity_id: str):
+    """Delete a amenity"""
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        return jsonify({"error": "Not found"}), 404
+    storage.delete(amenity)
+    storage.save()
+    return jsonify({}), 200
+
+
+@app_views.route("/amenities", methods=["POST"],
+                 strict_slashes=False)
+def add_amenity():
+    """Add a amenity"""
+
+    if not request.is_json:
+        return jsonify({"error": "Not a JSON"}), 400
+
+    data: dict = request.get_json()
+
+    if data.get("name") is None:
+        return jsonify({"error": "Missing name"}), 400
+
+    amenity = Amenity(**data)
+
+    amenity.save()
+
+    return jsonify(amenity.to_dict()), 201
+
+
+@app_views.route("/amenities/<amenity_id>", methods=["PUT"],
+                 strict_slashes=False)
+def update_state(amenity_id: str):
+    """Update a amenity"""
+
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        return jsonify({"error": "Not found"}), 404
+
+    if not request.is_json:
+        return jsonify({"error": "Not a JSON"}), 400
+
+    data: dict = request.get_json()
+
+    for key, value in data.items():
+        if key not in ["id", "created_at", "updated_at"]:
+            setattr(amenity, key, value)
+
+    amenity.save()
+
+    return jsonify(amenity.to_dict()), 200
